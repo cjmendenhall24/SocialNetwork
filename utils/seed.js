7918cfa02b0992bd0e78c04d9e74e5c5900097c3
CJ <cjmendenhall24@hotmail.com>
2023-11-12 16:39:36 -0500

    added seed file

---
+++ utils/seed.js
@@ -0,0 +1,21 @@
+const connection = require('../config/connection');
+const { User, Thought, Reaction } = require('../models');
+
+const users = [
+    {
+        username : 'Charles',
+        email: 'nitemare@hotmail.com'
+    },
+    {
+        username: 'Jack',
+        email: 'jb_smooth@hotmail.com'
+    },
+    {
+        username: 'Cam',
+        email: 'iwin123@hotmail.com'
+    },
+    {
+        username: 'Cal',
+        email: 'iluvmommy123@hotmail.com'
+    }
+]
